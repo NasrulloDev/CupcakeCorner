2a76d623ba4d438a20c946dc605d3969fdf1ef9c
NasrulloDev <ismoilzanovnasrullo@gmail.com>
2024-02-14 07:09:23 +0500

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Насрулло Исмоилжонов on 14/02/24.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
